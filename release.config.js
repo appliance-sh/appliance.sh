ece218f3b90ece0f0ce42c22d2e58794f748d4e6
Eliot Lim <eliot.lim@gmail.com>
2025-06-14 21:38:54 +0800

    fix: resolve semantic-release assets error

--- release.config.js
+++ release.config.js
@@ -6,15 +6,7 @@ module.exports = {
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        "assets": [
-          {"path": ["packages/cli/dist/**/*.js", "packages/cli/package.json"], "label": "Appliance CLI"},
-          {"path": ["packages/sdk/dist/**/*.js", "packages/sdk/package.json"], "label": "Appliance SDK"},
-        ]
-      }
-    ],
+    "@semantic-release/github",
     ["@semantic-release/exec", {
       "publishCmd": "echo \"publish_required=true\" >> \"$GITHUB_OUTPUT\""
     }],
