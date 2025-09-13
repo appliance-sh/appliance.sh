9b647aa556c73e758d030d95fadb3fdbbb98daf0
Eliot Lim <eliot.lim@gmail.com>
2025-09-13 22:29:12 +0800

    feat(sdk): add appliance manifest types

--- packages/cli/src/appliance-build.ts
+++ packages/cli/src/appliance-build.ts
@@ -3,7 +3,7 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .option('-f, --file <file>', 'appliance manifest file')
+  .option('-f, --file <file>', 'appliance manifest file', 'appliance.json')
   .option('-d, --directory <directory>', 'appliance directory');
 
 program.parse(process.argv);
