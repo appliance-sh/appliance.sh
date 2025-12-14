2d4829e7fca668c48d3aba994fdaed1328c7ed67
Eliot Lim <eliot.lim@gmail.com>
2025-12-14 23:11:53 +0800

    feat: add cdk experimental stacks

--- packages/install-aws/src/index.ts
+++ packages/install-aws/src/index.ts
@@ -5,10 +5,21 @@ export * from './version';
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ApplianceApiStackV1 } from './lib/ApplianceApiStackV1';
+import { ApplianceGlobalStackV1 } from './lib/ApplianceGlobalStackV1';
 
 const app = new cdk.App();
 
 const name = 'stack';
 const stackId = `appliance-api-${name}`;
 
-new ApplianceApiStackV1(app, stackId, {});
+const globalStack = new ApplianceGlobalStackV1(app, `${stackId}-global`, {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
+});
+
+if (!globalStack.hostedZone) throw new Error('Global stack not deployed');
+
+new ApplianceApiStackV1(app, stackId, {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  crossRegionReferences: true,
+  hostedZone: globalStack.hostedZone,
+});
---
+++ packages/install-aws/src/lib/ApplianceGlobalStackV1.ts
@@ -0,0 +1,72 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class ApplianceGlobalStackV1 extends cdk.Stack {
+  public hostedZone;
+  public apiLambda;
+  public apiLambdaUrl;
+  public globalCertificate;
+  public cfDistribution;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const stages = {
+      stage1: true, // Permanent constructs, like the DNS zone, VPC, and IAM roles
+      stage2: true,
+      stage3: true,
+      stage4: true,
+    };
+
+    const domainName = 'appliance.sh';
+
+    if (stages.stage1) {
+      this.hostedZone = new cdk.aws_route53.PublicHostedZone(this, `${id}-zone`, {
+        zoneName: domainName,
+      });
+
+      new cdk.CfnOutput(this, `${id}-zone-name`, {
+        value: this.hostedZone.zoneName,
+      });
+    }
+
+    if (stages.stage2 && this.hostedZone) {
+      this.globalCertificate = new cdk.aws_certificatemanager.Certificate(this, `${id}-certificate`, {
+        domainName: `*.${domainName}`,
+        validation: cdk.aws_certificatemanager.CertificateValidation.fromDns(this.hostedZone),
+        subjectAlternativeNames: [`*.${domainName}`],
+      });
+    }
+
+    if (stages.stage3 && this.globalCertificate) {
+      this.apiLambda = new cdk.aws_lambda.Function(this, `${id}-api-server`, {
+        runtime: cdk.aws_lambda.Runtime.NODEJS_24_X,
+        handler: 'index.handler',
+        code: cdk.aws_lambda.Code.fromInline('exports.handler = async () => "Hello World!";'),
+      });
+
+      this.apiLambdaUrl = this.apiLambda.addFunctionUrl({
+        authType: cdk.aws_lambda.FunctionUrlAuthType.AWS_IAM,
+      });
+
+      this.cfDistribution = new cdk.aws_cloudfront.Distribution(this, `${id}-distribution`, {
+        domainNames: [`*.${domainName}`],
+        certificate: this.globalCertificate,
+        defaultBehavior: {
+          origin: cdk.aws_cloudfront_origins.FunctionUrlOrigin.withOriginAccessControl(this.apiLambdaUrl),
+        },
+      });
+
+      new cdk.CfnOutput(this, `${id}-distribution-domain`, { value: this.cfDistribution.distributionDomainName });
+    }
+
+    if (stages.stage4 && this.hostedZone && this.cfDistribution) {
+      new cdk.aws_route53.CnameRecord(this, `${id}-cname`, {
+        zone: this.hostedZone,
+        recordName: `api.${domainName}`,
+        domainName: this.cfDistribution.distributionDomainName,
+        ttl: cdk.Duration.minutes(5),
+      });
+    }
+  }
+}
