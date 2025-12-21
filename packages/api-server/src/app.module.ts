e89f8bfaee95ebdadeed3ad5342b76913ddcd98a
Eliot Lim <eliotlim@users.noreply.github.com>
2025-12-21 20:54:59 +0800

    feat: add cloudfront and nestjs proof of concept (#21)
    * feat: create cloudfront tests
    
    * feat: add cdk nestjs tests
    
    * chore: fix lint
    
    * chore: fix lint again

--- packages/api-server/src/app.module.ts
+++ packages/api-server/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ApplianceStackModule } from './cdk/appliance-stack.module';
 
 @Module({
-  imports: [],
+  imports: [ApplianceStackModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- packages/install-aws/src/index.ts
+++ packages/install-aws/src/index.ts
@@ -1,25 +1,50 @@
 #!/usr/bin/env ts-node
 
+import { ApplianceApiGlobalStackV1 } from './lib/ApplianceApiGlobalStackV1';
+
 export * from './version';
 
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ApplianceApiStackV1 } from './lib/ApplianceApiStackV1';
 import { ApplianceGlobalStackV1 } from './lib/ApplianceGlobalStackV1';
+import { ApplianceBase } from './lib/ApplianceBase';
 
 const app = new cdk.App();
 
-const name = 'stack';
-const stackId = `appliance-api-${name}`;
+const stackId = `appliance-api-${`stack`}`;
+
+const base = new ApplianceBase(app, 'appliance-base', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
+
+const domainName = base.domainName;
 
 const globalStack = new ApplianceGlobalStackV1(app, `${stackId}-global`, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: 'us-east-1' },
+  domain: domainName.valueAsString,
+  crossRegionReferences: true,
 });
 
 if (!globalStack.hostedZone) throw new Error('Global stack not deployed');
+if (!globalStack.cfDistribution) throw new Error('Global stack not deployed');
+if (!globalStack.globalCertificate) throw new Error('Global stack not deployed');
 
-new ApplianceApiStackV1(app, stackId, {
+const localApiStack = new ApplianceApiStackV1(app, stackId, {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   crossRegionReferences: true,
   hostedZone: globalStack.hostedZone,
+  cfDistribution: globalStack.cfDistribution,
+  domain: domainName.valueAsString,
+});
+
+if (!localApiStack.apiLambdaUrl) throw new Error('Local API stack not deployed');
+
+new ApplianceApiGlobalStackV1(app, `${stackId}-api-global`, {
+  functionUrl: localApiStack.apiLambdaUrl,
+  globalCertificate: globalStack.globalCertificate,
+  hostedZone: globalStack.hostedZone,
+  cfDistribution: globalStack.cfDistribution,
+  crossRegionReferences: true,
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
